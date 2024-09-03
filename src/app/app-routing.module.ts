767d05e5d2598c1dbdf87cd1944f2b5cb453103a
Saikiran.Soma <saikiran.soma@ust.com>
2024-09-03 20:34:01 +0530

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
-  
+  {path:'home', component:HomeComponent},
+  //{path:'about', component:AboutComponent},
+  {path:'login', component:LoginComponent},
+  {path:'register', component:RegisterComponent},
+  //{path:'mainpage', component:MainpageComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  //{ path: '**', redirectTo: '/home' }
   
 ];
 
