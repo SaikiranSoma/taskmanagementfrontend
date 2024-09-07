1df828ac842e1c02560f761617e5a5037a03045e
Saikiran.Soma <saikiran.soma@ust.com>
2024-09-07 15:48:26 +0530

    Added routing to projects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateProjectComponent } from './components/create-project/create-project.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { MyProjectsComponent } from './components/my-projects/my-projects.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
@@ -11,6 +13,8 @@ const routes: Routes = [
   {path:'login', component:LoginComponent},
   {path:'register', component:RegisterComponent},
   {path:'dashboard', component:DashboardComponent},
+  {path:'create-project', component:CreateProjectComponent},
+  {path:'my-projects', component:MyProjectsComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' }
   //{ path: '**', redirectTo: '/home' }
   
