53a28d911cb8ca4ea49ba4528de0faa6651c2c2c
Saikiran.Soma <saikiran.soma@ust.com>
2024-09-07 15:33:52 +0530

    Added Dashboard structure

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -6,41 +6,5 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/dra
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  // todo = [
-  //   { name: 'Development', priority: 'High', status: 'On track' },
-  // ];
-
-  // inprogress = [
-  //   { name: 'Testing Project', priority: 'Medium', status: 'On track' },
-  // ];
-
-  // completed = [
-  //   { name: 'Testing', priority: 'High', status: 'At risk' },
-  // ];
-
-  // // Update method to type the event as CdkDragDrop
-  // drop(event: CdkDragDrop<any[]>) {
-  //   if (event.previousContainer === event.container) {
-  //     moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-  //   } else {
-  //     transferArrayItem(
-  //       event.previousContainer.data,
-  //       event.container.data,
-  //       event.previousIndex,
-  //       event.currentIndex,
-  //     );
-  //   }
-  // }
-
-  // // Method to add a new task to the appropriate array
-  // addTask(status: string) {
-  //   const newTask = { name: 'New Task', priority: 'Low', status: 'To be updated' };
-  //   if (status === 'todo') {
-  //     this.todo.push(newTask);
-  //   } else if (status === 'inprogress') {
-  //     this.inprogress.push(newTask);
-  //   } else if (status === 'completed') {
-  //     this.completed.push(newTask);
-  //   }
-  // }
+     userName:string='saikiran';
 }
